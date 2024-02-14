6239ff39ee9e9c2cf9700ee2b5c20447ac494f95
mahambach <ma.hambach@gmx.net>
2024-02-14 11:40:25 +0100

    add class "Message.java" add class "Controller.java"
    add path "/api/messages"
    add POST for "/api/messages"
    add GET for "/api/messages"

---
+++ src/main/java/com/example/challenge_2024_02_14_swing/controller/Controller.java
@@ -0,0 +1,35 @@
+package com.example.challenge_2024_02_14_swing.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/messages")
+public class Controller {
+    List<Message> messages = new ArrayList<>();
+
+    public Controller() {
+        messages.add(new Message("1", "John", "Hello World"));
+        messages.add(new Message("2", "Jane", "Hi there"));
+    }
+
+    @GetMapping
+    public List<Message> getMessages() {
+        return messages;
+    }
+    @PostMapping
+    public void addMessage(@RequestBody Message message){
+        messages.add(message);
+    }
+
+//    @PostMapping
+//    public Message postMessage(@RequestBody String id,
+//                               @RequestBody String name,
+//                               @RequestBody String message) {
+//        Message newMessage = new Message(id, name, message);
+//        messages.add(newMessage);
+//        return newMessage;
+//    }
+}
---
+++ src/main/java/com/example/challenge_2024_02_14_swing/controller/Message.java
@@ -0,0 +1,12 @@
+package com.example.challenge_2024_02_14_swing.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Message {
+    private String id;
+    private String name;
+    private String message;
+}
